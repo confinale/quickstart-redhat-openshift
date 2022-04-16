738fd8792f4332bf52ddf0b247f9e78a8b4e8f45
Florian Schrag <github@schrag.org>
2022-04-16 23:34:38 +0200

    fix kubeconfig reference

--- functions/source/OpenShiftResource/src/awsqs_openshift_manager/read.py
+++ functions/source/OpenShiftResource/src/awsqs_openshift_manager/read.py
@@ -58,7 +58,7 @@ def fetch_kube_parameters(model, session):
         else:
             LOG.debug('Fetching KubeConfig')
             model.KubeConfigArn = model.KubeConfig = \
-                secrets.describe_secret(SecretId=f'{model.InfrastructureId}-kubeconfig')['ARN']
+                secrets.describe_secret(SecretId=f'{model.ClusterName}-kubeconfig')['ARN']
 
     except ssm.exceptions.ParameterNotFound:
         err_msg = f"ERROR - Parameter at /OpenShift/{model.ClusterName}/InfrastructureId not found"
